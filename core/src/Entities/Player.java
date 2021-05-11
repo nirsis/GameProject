a1efadbb9a624035ca15b5e448103008bbe3c89d
Jeronimo Ferraro <61193611+nirsis@users.noreply.github.com>
2021-05-11 16:56:21 +0100

    start collisions

--- core/src/Entities/Player.java
+++ core/src/Entities/Player.java
@@ -32,6 +32,37 @@ public class Player extends Sprite {
 
         setX(getX()+movement.x);
         setY(getY()+movement.y);
+
+        //collision detection
+        float oldX = getX();
+        float oldY = getY();
+        boolean collisionX = false;
+        boolean collisionY = false;
+
+        /*Check areas of the map towards where the character is moving, if movement.x < 0 the character is moving left
+        if movement.x is < 0 the character is moving right, same for movemnt.y points towards top and bottom depending on the value.
+        This avoids checking the whole map at the same time */
+
+        if(movement.x < 0) {
+
+            //check top left
+
+          //  collisionX =.getCell(getX(), getY()+getHeight());
+
+
+
+
+        }
+        else if(movement.x > 0) {
+
+        }
+
+        if(movement.y < 0) {
+
+        }
+        else if(movement.y > 0) {
+
+        }
     }
 
 }
--- core/src/Scenes/Gameplay.java
+++ core/src/Scenes/Gameplay.java
@@ -15,6 +15,7 @@ import Entities.Player;
 
 
 public class Gameplay implements Screen {
+
     private String hello = "Hello";
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;

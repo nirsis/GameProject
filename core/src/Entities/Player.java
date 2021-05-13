0341721f0e979c03d5ce40d2782057cb3dd0de93
Jeronimo Ferraro <61193611+nirsis@users.noreply.github.com>
2021-05-13 06:12:02 +0100

    RenderProblems

--- core/src/Entities/Player.java
+++ core/src/Entities/Player.java
@@ -1,44 +1,139 @@
 package Entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Batch;
+
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class Player extends Sprite {
+import Scenes.Gameplay;
+
+public class Player extends Sprite implements InputProcessor {
 
     private Vector2 movement = new Vector2();
-    private float speed = 60*2;
+    private float speed = 500;
     private TiledMapTileLayer staticLayer;
+    private Gameplay gp = new Gameplay();
+    private TiledMap map;
 
 
 
     public Player(Sprite sprite, TiledMapTileLayer staticLayer) {
 
-        super(sprite);
-        this.staticLayer = staticLayer;
+    super(sprite);
+    this.staticLayer = staticLayer;
 
-    }
+  }
 
-    @Override
-    public void draw(Batch batch) {
-
-        super.draw(batch);
-    }
 
     public void draw(SpriteBatch spriteBatch) {
 
-        update(Gdx.graphics.getDeltaTime());
-        super.draw(spriteBatch);
+    update(Gdx.graphics.getDeltaTime());
+    super.draw(spriteBatch);
 
     }
 
+
+   @Override
+   public void draw(Batch batch) {
+
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(batch);
+}
+/*
+     Code from  Java Tiled Map Game (LibGDX) | Episode 4 update - better collision detection implementation
+     Source: https://www.youtube.com/watch?v=TLZbC9brH1c&list=PLXY8okVWvwZ0qmqSBhOtqYRjzWtUCWylb&index=5&ab_channel=dermetfan
+ */
+
+public boolean isBlocked (float x, float y) {
+
+    boolean toReturn = false;
+    TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer)map.getLayers().get("ObjectLayer");
+    MapObjects objects = collisionObjectLayer.getObjects();
+    for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+
+        Rectangle rectangle = rectangleObject.getRectangle();
+        if (Intersector.overlaps(rectangle, getBoundingRectangle())) {
+            toReturn = true;
+
+        }
+    }
+//    TiledMapTileLayer.Cell cell = staticLayer.getCell((int)(x / staticLayer.getTileWidth()), (int) (y / staticLayer.getTileHeight()));
+//    System.out.println();
+//  return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("Blocked");
+    return toReturn;
+}
+
+public boolean collidesRight() {
+    boolean collision = false;
+
+    for(float i = 0; i< getHeight(); i += staticLayer.getTileHeight()/2) {
+
+        if(collision = isBlocked(getX()+getWidth(), getY()+ i)) {
+            break;
+        }
+
+    }
+
+    return collision;
+
+}
+public boolean collidesLeft() {
+    boolean collision = false;
+
+    for(float i = 0; i< getHeight(); i += staticLayer.getTileHeight()/2) {
+
+        if(collision = isBlocked(getX(), getY()+ i)) {
+            break;
+        }
+
+    }
+
+    return collision;
+
+}
+public boolean collidesTop() {
+    boolean collision = false;
+
+    for(float i = 0; i< getWidth(); i += staticLayer.getTileWidth()/2) {
+
+        if(collision = isBlocked(getX()+ i, getY()+getHeight())) {
+            break;
+        }
+
+    }
+
+    return collision;
+
+}
+public boolean collidesBottom() {
+    boolean collision = false;
+
+    for(float i = 0; i< getWidth(); i += staticLayer.getTileWidth()/2) {
+
+        if(collision = isBlocked(getX()+ i, getY())) {
+            break;
+        }
+
+    }
+
+    return collision;
+
+}
     public void update(float delta) {
 
-        setX(getX()+movement.x);
-        setY(getY()+movement.y);
+        setX(getX()+movement.x * delta);
+        setY(getY()+movement.y * delta);
 
         //collision detection
         float oldX = getX();
@@ -61,75 +156,30 @@ public class Player extends Sprite {
         If colX/Y is true, further movement will be denied in that direction*/
 
         if(movement.x < 0) {
-
-            //check left tile above the object
-            colX = staticLayer.getCell((int)(getX()/tileW), (int)((getY()+getHeight()) / tileH)).getTile().getProperties().containsKey("Blocked");
-
-            //Only check for middle and bottom if the colX is still false
-            if(!colX) {
-                //check left tile at the object position
-                colX = staticLayer.getCell((int) (getX() / tileW), (int) ((getY() + getHeight() / 2) / tileH)).getTile().getProperties().containsKey("Blocked");
-            }
-
-            if(!colX) {
-                //check left tile under the object
-                colX = staticLayer.getCell((int) (getX() / tileW), (int) (getY() / tileH)).getTile().getProperties().containsKey("Blocked");
-            }
+            //Check left side
+            colX = collidesLeft();
+        }else if(movement.x > 0) {
+            //Check right side
+            colX = collidesRight();
         }
-        else if(movement.x > 0) {
 
-            //check right tile above the object
-            colX = staticLayer.getCell((int)((getX()+getWidth())/tileW), (int)((getY()+getHeight())/tileH)).getTile().getProperties().containsKey("Blocked");
-
-            //Only check for middle and bottom if the colX is still false
-            if(!colX) {
-
-                //check right tile at the object position
-                colX = staticLayer.getCell((int)((getX()+getWidth())/tileW), (int) ((getY() + getHeight() / 2) / tileH)).getTile().getProperties().containsKey("Blocked");
-            }
-
-            if(!colX) {
-
-                //check right tile under the object
-                colX = staticLayer.getCell((int)((getX()+getWidth())/tileW), (int) (getY() / tileH)).getTile().getProperties().containsKey("Blocked");
-            }
-        }
-        if(colX == true) {
+        // If there is a collision, set the position back to the old position and stop movement
+        if(colX) {
             setX(oldX);
             movement.x = 0;
         }
 
 
         if(movement.y < 0) {
-
-            //start checking bottom left tile
-            colY = staticLayer.getCell((int) (getX()/ tileW), (int) (getY()/tileH)).getTile().getProperties().containsKey("Blocked");
-
-            //if still false, check straight under and then bottom right
-            if(!colY) {
-            colY = staticLayer.getCell((int) ((getX()+getWidth()/2)/tileW), (int) (getY()/tileH)).getTile().getProperties().containsKey("Blocked");
-            }
-            if(!colY) {
-                colY = staticLayer.getCell((int) ((getX()+getWidth())/tileW), (int) (getY()/tileH)).getTile().getProperties().containsKey("Blocked");
-            }
+            //Check bottom side
+            colY = collidesBottom();
         }
         else if(movement.y > 0) {
-        //start checking bottom left tile
-        colY = staticLayer.getCell((int) (getX()/ tileW), (int) ((getY()+getHeight())/tileH)).getTile().getProperties().containsKey("Blocked");
-
-        //if still false, check straight above and then top right
-        if(!colY) {
-
-        colY = staticLayer.getCell((int) ((getX()+getWidth()/2)/tileW), (int) ((getY()+getHeight())/tileH)).getTile().getProperties().containsKey("Blocked");
-
-        }
-        if(!colY) {
-
-        colY = staticLayer.getCell((int) ((getX()+getWidth())/tileW), (int) ((getY()+getHeight())/tileH)).getTile().getProperties().containsKey("Blocked");
-
-        }
+            //Check top side
+            colY = collidesTop();
         }
 
+        // If there is a collision, set the position back to the old position and stop movement
         if(colY == true) {
             setY(oldY);
             movement.y = 0;
@@ -157,5 +207,77 @@ public class Player extends Sprite {
 
     public void setStaticLayer(TiledMapTileLayer staticLayer) {
         this.staticLayer = staticLayer;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.W || keycode == Input.Keys.UP) {
+            movement.y = speed;
+
+        }else if(keycode == Input.Keys.A || keycode == Input.Keys.LEFT) {
+            movement.x = -speed;
+
+        }else if(keycode == Input.Keys.D || keycode == Input.Keys.RIGHT){
+            movement.x = speed;
+
+        }else if(keycode == Input.Keys.S || keycode == Input.Keys.DOWN) {
+            movement.y = -speed;
+
+        }else if(keycode == Input.Keys.SPACE) {
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if(keycode == Input.Keys.W || keycode == Input.Keys.UP) {
+            movement.y = 0;
+
+        }else if(keycode == Input.Keys.A || keycode == Input.Keys.LEFT) {
+            movement.x = 0;
+
+        }else if(keycode == Input.Keys.D || keycode == Input.Keys.RIGHT){
+            movement.x = 0;
+
+        }else if(keycode == Input.Keys.S || keycode == Input.Keys.DOWN) {
+            movement.y = 0;
+
+        }else if(keycode == Input.Keys.SPACE) {
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
     }
 }
--- core/src/Scenes/Gameplay.java
+++ core/src/Scenes/Gameplay.java
@@ -7,32 +7,70 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
 
 import Entities.Player;
 
 
 public class Gameplay implements Screen {
 
-    private String hello = "Hello";
+
+    public TiledMap getMap() {
+        return map;
+    }
+
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
+    private ShapeRenderer shapeRenderer;
     private Player player;
+
+    public Gameplay() {
+
+    }
 
     @Override
     public void render(float delta) {
 
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.position.set(player.getX()+player.getWidth()/2, player.getY()+player.getHeight()/2, 0);
+        camera.update();
+
+
+
         renderer.setView(camera);
-        renderer.render();
+
+        //Render First layer of the map
+        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("Map"));
+        //Render player
         renderer.getBatch().begin();
         player.draw(renderer.getBatch());
         renderer.getBatch().end();
+        //Render Second layer of the map
+        renderer.renderTileLayer((TiledMapTileLayer) map.getLayers().get("Static"));
+
+
+
+        for(MapObject object : map.getLayers().get("ObjectLayer").getObjects()) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.rect(rect.x, rect.y, rect.width, rect.height);
+            shapeRenderer.end();
+        }
+
+
+
 
     }
 
@@ -41,7 +79,7 @@ public class Gameplay implements Screen {
 
         camera.viewportWidth = width;
         camera.viewportHeight = height;
-        camera.update();
+
 
     }
 
@@ -51,9 +89,12 @@ public class Gameplay implements Screen {
         TmxMapLoader loader = new TmxMapLoader();
         map = loader.load("maps/themap.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
+        shapeRenderer = new ShapeRenderer();
         camera = new OrthographicCamera();
         player = new Player(new Sprite(new Texture("players/Swordsman.png")),(TiledMapTileLayer)map.getLayers().get(0));
-        player.setPosition(15*player.getStaticLayer().getTileWidth(), 15 * player.getStaticLayer().getTileHeight());
+        player.setPosition(1500 , 15 );
+
+        Gdx.input.setInputProcessor(player);
     }
 
 
